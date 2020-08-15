70382d2aae64366b900493bc125191a19e4291ea
rogerio-romao <58592935+rogerio-romao@users.noreply.github.com>
2020-08-15 03:49:39 +0100

    h1 text and hr on featured page

--- src/views/Featured.vue
+++ src/views/Featured.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="featured">
     <div class="cards border border-red-600 theme-bg p-4">
+      <h1 class="text-center text-gray-600 text-2xl font-bold my-2">
+        Featured Projects
+      </h1>
+      <p class="text-center text-gray-600 mb-2">
+        A selection of some of my best projects. See the portfolio page for a
+        bigger collection of works.
+      </p>
+      <hr />
       <FeaturedCard />
     </div>
   </div>
@@ -24,7 +32,9 @@ export default {
   padding: 0 1.5em;
   background-color: var(--secondary-bg);
 }
-
+hr {
+  margin: 20px 50px;
+}
 .card {
   margin: 1em;
   display: inline-block;
