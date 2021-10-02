4508d0e47c5b4f0de845e867a2e5c7e601addfb1
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-10-02 20:50:03 +0100

    removed some projects from featured

--- src/store/featured.js
+++ src/store/featured.js
@@ -13,16 +13,6 @@ const featured = {
       },
       {
         id: 2,
-        name: "The Spiral",
-        image: require("../assets/spiral.png"),
-        shortDesc: "A JS music player / visualiser / generative art.",
-        longDesc:
-          "Very proud of this one, programmed from scratch, pushing the envelope with animation and canvas, custom functions including a stagger using requestanimationframe, and a custom music player. Won a Codepen Pick! Also works as a standalone desktop app, planning to have this for sale in App Store.",
-        source: "https://codepen.io/rogerio-romao/pen/mdevOwm",
-        live: "https://codepen.io/rogerio-romao/full/mdevOwm"
-      },
-      {
-        id: 3,
         name: "Online Portfolio/CV",
         image: require("../assets/portfolio.png"),
         shortDesc: "My online portfolio / CV website",
@@ -32,24 +22,34 @@ const featured = {
         live: "#"
       },
       {
-        id: 4,
-        name: "Coffee Shop",
-        image: require("../assets/coffee-shop.png"),
-        shortDesc: "A shop where you can buy coffee products",
+        id: 3,
+        name: "Idle DJ Game",
+        image: require("../assets/idle-dj.png"),
+        shortDesc: "Idle / clicker game made with Vue and Vuex.",
         longDesc:
-          "This is an e-commerce website made on Gatsby and React, with a GraphQL backend, that uses the Contentful API for the content management. It has continuous integration when deploying to Github via Netlify and also a webhook setup where if the user changes only the Contentful data and not the code itself, a build is triggered on Netlify. The cart is an implementation of SnipCart.",
-        source: "https://github.com/rogerio-romao/gatsby-coffee-project",
-        live: "https://elated-wing-b66940.netlify.app/"
+          "Still a work in progress, the idea here, was to experiment using Vuex to the full capacity. There is a tremendous amount of state and inter-dependencies, extensive use of modules, actions and mutations.",
+        source: "https://github.com/rogerio-romao/idle_dj",
+        live: "https://silly-lamport-fbfed0.netlify.app/"
+      },
+      {
+        id: 4,
+        name: "The Spiral",
+        image: require("../assets/spiral.png"),
+        shortDesc: "A JS music player / visualiser / generative art.",
+        longDesc:
+          "Very proud of this one, programmed from scratch, pushing the envelope with animation and canvas, custom functions including a stagger using requestanimationframe, and a custom music player. Won a Codepen Pick! Also works as a standalone desktop app, planning to have this for sale in App Store.",
+        source: "https://codepen.io/rogerio-romao/pen/mdevOwm",
+        live: "https://codepen.io/rogerio-romao/full/mdevOwm"
       },
       {
         id: 5,
-        name: "The Catstagram",
-        image: require("../assets/catstagram.png"),
-        shortDesc: "A mockup of an Instagram like app.",
+        name: "Netflix Clone",
+        image: require("../assets/netflix-clone.png"),
+        shortDesc: "Netflix clone using TMDB API with movie trailer previews.",
         longDesc:
-          "This was done with Vue as the framework with Nuxt bootstrapping and Vuetify for the presentation. Uses async API calls via Axios.",
-        source: "https://github.com/rogerio-romao/purple-test",
-        live: "https://nifty-ptolemy-7b0487.netlify.app/"
+          "This is a React application that is hosted on Firebase, done using Hooks and functional components. The data is coming from the TMDB API, and each title can be clicked to watch a movie preview.",
+        source: "https://github.com/rogerio-romao/netflix-clone-react",
+        live: "https://netflix-clone-f2569.web.app/"
       },
       {
         id: 6,
@@ -94,46 +94,6 @@ const featured = {
       },
       {
         id: 10,
-        name: "Password Generator",
-        image: require("../assets/password-gen.png"),
-        shortDesc: "Create random secure pesswords.",
-        longDesc:
-          "Constructed using Vue and Vuetify. Select the length of the password you want to generate and if you want numbers and/or special characters included. You also get a handy button to copy the password to clipboard.",
-        source: "https://codepen.io/rogerio-romao/pen/poyoWKw",
-        live: "https://codepen.io/rogerio-romao/full/poyoWKw"
-      },
-      {
-        id: 11,
-        name: "YelpCamp",
-        image: require("../assets/yelpcamp.png"),
-        shortDesc: "A Yelp like website from camping grounds.",
-        longDesc:
-          "This is a full fledged full-stack project that as the assignment from Colt Steele's WebDev Bootcamp. It's got API's, geocoding, restful routes, mvc architecture, templating, the works.",
-        source: "",
-        live: "https://dry-escarpment-99868.herokuapp.com/"
-      },
-      {
-        id: 12,
-        name: "The Collatz Sequence",
-        image: require("../assets/collatz.png"),
-        shortDesc: "A visualizer tool for the famous math sequence.",
-        longDesc:
-          "This was mainly about trying to develop working with canvas, animation and requestanimationframe. I used a math sequence as the base to program a graph engine on canvas that adjusts to the kind of data it is receiving. Fully commented out code as to how it works.",
-        source: "https://codepen.io/rogerio-romao/pen/WNQxaWq",
-        live: "https://codepen.io/rogerio-romao/full/WNQxaWq"
-      },
-      {
-        id: 13,
-        name: "Avatars",
-        image: require("../assets/avatars.png"),
-        shortDesc: "A profile image / avatar custom display study.",
-        longDesc:
-          "Codepen has these weekly themed challenges. This was my entry for a challenge on how to create a user profile 'avatar' component that would work with different sizes and types of images and i like the result very much.",
-        source: "https://codepen.io/rogerio-romao/pen/zYvzmRp",
-        live: "https://codepen.io/rogerio-romao/full/zYvzmRp"
-      },
-      {
-        id: 14,
         name: "Game of Life",
         image: require("../assets/gol.png"),
         shortDesc: "My version of the classic Conway's Game of Life.",
@@ -141,26 +101,6 @@ const featured = {
           "This was an entry for a Game of Life challenge in Codepen. Programmed a version of the classic game of life, with a feature to draw your own patterns, and learned a lot while doing so!",
         source: "https://codepen.io/rogerio-romao/pen/VwvbdZE",
         live: "https://codepen.io/rogerio-romao/full/VwvbdZE"
-      },
-      {
-        id: 15,
-        name: "Netflix Clone",
-        image: require("../assets/netflix-clone.png"),
-        shortDesc: "Netflix clone using TMDB API with movie trailer previews.",
-        longDesc:
-          "This is a React application that is hosted on Firebase, done using Hooks and functional components. The data is coming from the TMDB API, and each title can be clicked to watch a movie preview.",
-        source: "https://github.com/rogerio-romao/netflix-clone-react",
-        live: "https://netflix-clone-f2569.web.app/"
-      },
-      {
-        id: 16,
-        name: "Idle DJ Game",
-        image: require("../assets/idle-dj.png"),
-        shortDesc: "Idle / clicker game made with Vue and Vuex.",
-        longDesc:
-          "Still a work in progress, the idea here, was to experiment using Vuex to the full capacity. There is a tremendous amount of state and inter-dependencies, extensive use of modules, actions and mutations.",
-        source: "https://github.com/rogerio-romao/idle_dj",
-        live: "https://silly-lamport-fbfed0.netlify.app/"
       }
     ]
   })
